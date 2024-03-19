56e73bbc90cd7d004a6d13fcc1c1af701d797885
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-03-19 20:34:17 +0530

    counter app

--- introd/Introd/src/App.jsx
+++ introd/Introd/src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {useState} from "react";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+
+  const [count,setcount]=useState(0);
+
+  function onclickhandler(){
+    setcount(count+1);
+  }
 
   return (
-    <>
-      
-      <h1>Vite + React</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-    </>
+    <div>
+    <button onClick={onclickhandler}>Counter {count}</button>
+    </div>
+
   )
 }
-console.log("How to get a job")
 
 export default App
