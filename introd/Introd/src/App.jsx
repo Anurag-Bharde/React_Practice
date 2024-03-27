d27d69916f20681c211e2853ef4d576bd81c6233
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-03-27 13:53:26 +0530

    useState and Components

--- introd/Introd/src/App.jsx
+++ introd/Introd/src/App.jsx
@@ -1,32 +1,24 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import {useState} from "react";
-import './App.css'
+// import './App.css'
 
 function App(){
 
   const [count,setCount]=useState(0);
-  return (
-    <div>
-    <CustomButton count={count} setCount={setCount}></CustomButton>
-    <CustomButton count={count+1} setCount={setCount}></CustomButton>
-    <CustomButton count={count-1} setCount={setCount}></CustomButton>
-    <CustomButton count={count*5} setCount={setCount}></CustomButton>
-    <CustomButton count={count%5} setCount={setCount}></CustomButton>
-    </div>
 
-  )
-}
-
-//Component
-function CustomButton(props){
-
-  function clickfunc(){
-    props.setCount(props.count+1)
-  }
   return(
-    <button onClick={clickfunc}>Counter {props.count}</button>
+    <div>
+      <CustomButton count={count} setCount={setCount}></CustomButton>
+    </div>
   )
 }
+  function CustomButton(props){
+
+    function onclickhandler(){
+      props.setCount(props.count+1);
+    }
+    return <button onClick={onclickhandler}> Counter {props.count}</button>
+  }
 
 export default App
